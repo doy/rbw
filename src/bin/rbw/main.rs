e5ccbac220b1f14e3cede1b55bad92a4f537c8c9
Jesse Luehrs <doy@tozt.net>
2020-04-18 05:15:34 -0400

    enable env_logger in the main app

--- src/bin/rbw/main.rs
+++ src/bin/rbw/main.rs
@@ -5,6 +5,11 @@ mod commands;
 mod sock;
 
 fn main() {
+    env_logger::from_env(
+        env_logger::Env::default().default_filter_or("info"),
+    )
+    .init();
+
     let matches = clap::App::new("rbw")
         .about("unofficial bitwarden cli")
         .author(clap::crate_authors!())
