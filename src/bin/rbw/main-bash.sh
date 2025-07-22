0ccb79aee4112a75516e5e9535a7f54234e9d954
Jesse Luehrs <doy@tozt.net>
2025-07-22 03:05:56 -0400

    improve bash and zsh completions a bit

--- src/bin/rbw/main-bash.sh
+++ src/bin/rbw/main-bash.sh
@@ -1,23 +1,41 @@
 _rbw_wrapper() {
-  local cur prev folder opts res
+  local cur prev folder opts res name user
   COMPREPLY=()
 
-  if [[ "${COMP_WORDS[1]}" == "get" ]]; then
-
-    # rbw get
+  if [[ "${COMP_WORDS[1]}" == "get" ]] && [[ $COMP_CWORD -gt 1 ]]; then
+    for (( i=2; i < COMP_CWORD; i++ )); do
+      case "${COMP_WORDS[i]}" in
+        --folder|-f|--field)
+          (( i++ ))
+          if [ "${COMP_WORDS[i]}" == "=" ]; then
+            (( i++ ))
+          fi
+          ;;
+        -*)
+          ;;
+        *)
+          if [ -z "$name" ]; then
+            name="${COMP_WORDS[i]}"
+          else
+            user="${COMP_WORDS[i]}"
+            break
+          fi
+          ;;
+      esac
+    done
 
     cur="${COMP_WORDS[COMP_CWORD]}"
     prev="${COMP_WORDS[COMP_CWORD-1]}"
 
-    for (( i=1; i < ${#COMP_WORDS[@]}; i++ )); do
+    for (( i=2; i < ${#COMP_WORDS[@]}; i++ )); do
       if [[ "${COMP_WORDS[i-2]}" == "--folder" ]] && [[ "${COMP_WORDS[i-1]}" == "=" ]]; then
         folder="${COMP_WORDS[i]}"
-        if [[ $i -eq $(( COMP_CWORD - 1 )) ]]; then
+        if [[ $i -eq $COMP_CWORD ]]; then
           prev="--folder"
         fi
       elif [[ "${COMP_WORDS[i-1]}" == "--folder" ]] && [[ "${COMP_WORDS[i]}" == "=" ]]; then
         folder=""
-        if [[ $i -eq $(( COMP_CWORD - 1 )) ]]; then
+        if [[ $i -eq $COMP_CWORD ]]; then
           prev="--folder"
           cur=""
         fi
@@ -30,26 +48,44 @@ _rbw_wrapper() {
       # rbw get --folder $folder
       res=$(
         rbw list --fields folder 2>/dev/null \
-          | awk -v folder="$folder" 'NF && $1 ~ folder {print $1}'
+          | awk -v folder="$folder" 'NF && $1 ~ folder && !a[$1]++ {print $1}' 2>/dev/null
       )
-
-      # Split output into an array (one entry per line)
-    elif [[ "$prev" != ^-- ]]; then
-      # rbw get ... $cur
-      res=$(
-        rbw list --fields name,folder 2>/dev/null \
-          | awk -F'\t' -v name="$cur" -v folder="$folder" '$1 ~ name && $1 && $2 ~ folder {print $1}'
-      )
+    elif [[ "$prev" != --field ]]; then
+      if [ -z "$name" ]; then
+        # rbw get ... $cur
+        res=$(
+          rbw list --fields name,folder 2>/dev/null \
+            | awk -F'\t' -v folder="$folder" '$1 && (!folder || $2 == folder) {print $1}' 2>/dev/null
+        )
+      elif [ -z "$user" ]; then
+        # rbw get ... name $cur
+        res=$(
+          rbw list --fields name,folder,user 2>/dev/null \
+            | awk -F'\t' -v name="$name" -v folder="$folder" '$1 == name && (!folder || $2 == folder) {print $3}' 2>/dev/null
+        )
+      fi
     else
-      return 0
+      _rbw "$@"
+      return
+    fi
+
+    if [[ "$cur" == -* ]]; then
+      res="-f --field --full --raw --clipboard -i --ignorecase -h --help $res"
+      if [ -z "$folder" ]; then
+        res="--folder $res"
+      fi
     fi
 
     mapfile -t opts <<< "$res"
     COMPREPLY=( $(compgen -W "${opts[*]}" -- "$cur") )
     return 0
   else
-    return 0
+    _rbw "$@"
   fi
 }
 
-complete -F _rbw_wrapper rbw
+if [[ "${BASH_VERSINFO[0]}" -eq 4 && "${BASH_VERSINFO[1]}" -ge 4 || "${BASH_VERSINFO[0]}" -gt 4 ]]; then
+    complete -F _rbw_wrapper -o nosort -o bashdefault -o default rbw
+else
+    complete -F _rbw_wrapper -o bashdefault -o default rbw
+fi
--- src/bin/rbw/main-zsh.sh
+++ src/bin/rbw/main-zsh.sh
@@ -1,63 +1,80 @@
 _rbw_wrapper() {
   local -a opts
-  local cur prev folder res
+  local cur prev folder res name user
 
-  if [[ "${words[2]}" == "get" ]]; then
+  if [[ "${(Q)words[2]}" == "get" ]] && [[ $CURRENT -gt 2 ]]; then
+    for (( i=3; i < CURRENT; i++ )); do
+      cur="${(Q)words[i]}"
 
-    # rbw get
+      case "$cur" in
+        --folder|-f|--field)
+          (( i++ ))
+          ;;
+        -*)
+          ;;
+        *)
+          if [ -z "$name" ]; then
+            name="$cur"
+          else
+            user="$cur"
+            break
+          fi
+          ;;
+      esac
+    done
 
-    for (( i=1; i < ${#words}; i++ )); do
-      cur="${words[$i]}"
-      if [[ "$cur" == --folder=* ]]; then
-        # rbw get ... --folder $folder
-        folder="${cur#--folder=}"
-        folder="${(Q)folder}"
-      elif [[ "${words[$(( i - 1 ))]}" == --folder ]]; then
-        # rbw get ... --folder $folder
-        folder="${(Q)words[$i]}"
-        prefix=""
+    cur="${words[CURRENT]}"
+    prev="${words[CURRENT-1]}"
+
+    for (( i=3; i <= ${#words}; i++ )); do
+      if [[ "${(Q)words[i-1]}" == "--folder" ]]; then
+        folder="${(Q)words[i]}"
+      elif [[ "${(Q)words[i]}" == "--folder="* ]]; then
+        folder="${(Q)words[i]#--folder=}"
       fi
     done
 
-    local cur="${(Q)words[CURRENT]}"
-
-    if [[ "$cur" == "--folder"* ]]; then
-      # rbw get --folder=$folder
-      if [[ "$cur" != "--folder" ]]; then
-        folder="${cur#--folder=}"
-        folder="${(Q)folder}"
-      fi
-      res=$(
-        rbw list --fields folder \
-          | awk -v folder="$folder" 'NF && $1 ~ folder {print "--folder=" $1}'
-         )
-    elif [[ "$prev" == "--folder" ]]; then
+    if [[ "$prev" == "--folder" ]] || [[ "$cur" == "--folder="* ]]; then
       # rbw get --folder $folder
       res=$(
-        rbw list --fields folder \
-          | awk -v folder="$folder" 'NF && $1 ~ folder {print $1}'
+        rbw list --fields folder 2>/dev/null \
+          | awk -v folder="$folder" 'NF && $1 ~ folder {print $1}' 2>/dev/null
       )
-    elif [[ "$prev" != -* ]] && [[ "$cur" != -* ]]; then
-      # rbw get ... $cur
-      res=$(
-        rbw list --fields name,folder \
-        | awk -F'\t' -v name="$cur" -v folder="$folder" '$1 ~ name && $1 && $2 ~ folder {print $1}'
-      )
+    elif [[ "$prev" != --field ]]; then
+      if [ -z "$name" ]; then
+        # rbw get ... $cur
+        res=$(
+          rbw list --fields name,folder 2>/dev/null \
+            | awk -F'\t' -v folder="$folder" '$1 && (!folder || $2 == folder) {print $1}' 2>/dev/null
+        )
+      elif [ -z "$user" ]; then
+        # rbw get ... name $cur
+        res=$(
+          rbw list --fields name,folder,user 2>/dev/null \
+            | awk -F'\t' -v name="$name" -v folder="$folder" '$1 == name && (!folder || $2 == folder) {print $3}' 2>/dev/null
+        )
+      fi
     else
       _rbw
       return
     fi
 
-    if [[ "$res" == "$cur" ]]; then
-      compadd -S '' -- "${opts[@]}"
+    if [[ "$cur" == -* ]] && [[ "$cur" != "--folder="* ]]; then
+      res=$'-f\n--field\n--full\n--raw\n--clipboard\n-i\n--ignorecase\n-h\n--help\n'"$res"
+      if [ -z "$folder" ]; then
+        res=$'--folder\n'"$res"
+      fi
+    fi
+
+    opts=("${(@f)${res}}")
+    if [[ "$cur" == "--folder="* ]]; then
+      compadd -P '--folder=' -- "${opts[@]}"
     else
-      opts=("${(@f)${res}}")
-      compadd -S '' -- "${opts[@]}"
+      compadd -- "${opts[@]}"
     fi
   else
     _rbw
   fi
-
 }
 
 compdef _rbw_wrapper rbw
