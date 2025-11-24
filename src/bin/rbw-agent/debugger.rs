3ee4ce1f1d04fe1e91bc0733e0bbdc5e68c1d98d
Antoine Carnec <antoinecarnec@gmail.com>
2025-11-24 20:57:28 +0000

    deny ptracing and core dumping for macos

--- src/bin/rbw-agent/debugger.rs
+++ src/bin/rbw-agent/debugger.rs
@@ -17,7 +17,37 @@ pub fn disable_tracing() -> anyhow::Result<()> {
     }
 }
 
-#[cfg(not(target_os = "linux"))]
+#[cfg(target_os = "macos")]
+pub fn disable_tracing() -> anyhow::Result<()> {
+    // safety: correct arguments to ptrace
+    // https://developer.apple.com/library/archive/documentation/System/Conceptual/ManPages_iPhoneOS/man2/ptrace.2.html
+    let ret = unsafe { libc::ptrace(libc::PT_DENY_ATTACH, 0, std::ptr::null_mut(), 0) };
+    if ret != 0 {
+        let e = std::io::Error::last_os_error();
+        return Err(anyhow::anyhow!(
+            "failed to deny debugger attach, agent memory may be readable by other processes: {}", e
+        ));
+    }
+
+    // disable core dumps
+    let rlim = libc::rlimit {
+        rlim_cur: 0,
+        rlim_max: 0,
+    };
+    // safety: correct argument
+    // https://developer.apple.com/library/archive/documentation/System/Conceptual/ManPages_iPhoneOS/man2/setrlimit.2.html
+    let ret = unsafe { libc::setrlimit(libc::RLIMIT_CORE, &rlim) };
+    if ret != 0 {
+        let e = std::io::Error::last_os_error();
+        return Err(anyhow::anyhow!(
+            "failed to disable core dumps, agent memory may be dumped to disk: {}", e
+        ));
+    }
+
+    Ok(())
+}
+
+#[cfg(not(any(target_os = "linux", target_os = "macos")))]
 pub fn disable_tracing() -> anyhow::Result<()> {
     Err(anyhow::anyhow!("failed to disable PTRACE_ATTACH, agent memory may be dumpable by other processes: unimplemented on this platform"))
 }
